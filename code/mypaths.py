2fc307852ff7b9e8e11f46255ace605eea7bc22c
Dennis Sergeev <dennis.sergeev@gmail.com>
2018-03-16 16:11:34 +0000

    change data path

--- code/mypaths.py
+++ code/mypaths.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Paths to data
-Depends on path.py package
 """
 import os
 from pathlib import Path
 
 # Root of the present repository
 topdir = Path('.').absolute().parent
+datadir = topdir/'data'
 
-# Data directories
+# External data directories
 igp_data_dir = Path('/media')/os.getenv('USER')/'Elements'/'IGP'/'data'/'total_backup_Alliance_20180308'
 
 # Output directories
